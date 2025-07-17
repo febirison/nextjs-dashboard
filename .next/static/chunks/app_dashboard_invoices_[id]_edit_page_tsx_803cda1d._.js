0455f7135e612218bbdf6fb95fb51cb2b5105a79
febirison <andersonokai@gmail.com>
2025-07-17 21:30:07 +0000

    Add error handling to invoices page for 'Something went wrong' issue

--- .next/static/chunks/app_dashboard_invoices_[id]_edit_page_tsx_803cda1d._.js
+++ .next/static/chunks/app_dashboard_invoices_[id]_edit_page_tsx_803cda1d._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_9779b989._.js"
+  "static/chunks/node_modules__pnpm_d62683aa._.js",
+  "static/chunks/app_6b215323._.js"
 ],
     source: "dynamic"
 });
