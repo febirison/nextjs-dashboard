22b0a3044da918998cbf39faa2f6b02604f85a8b
febirison <andersonokai@gmail.com>
2025-07-26 04:22:52 +0000

    update

--- .next/static/chunks/app_dashboard_invoices_error_tsx_987e0dff._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b400c20d._.js"
-],
-    source: "dynamic"
-});
